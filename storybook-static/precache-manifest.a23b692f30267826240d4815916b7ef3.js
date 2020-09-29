00b3d8fd086291cef745ee034b8139ded1633674
s_chai <s_chai@dreamarts.co.jp>
2020-09-29 11:35:09 +0900

    taskbox UI

---
+++ storybook-static/precache-manifest.a23b692f30267826240d4815916b7ef3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfff32351de721fb3f753bcf29855974",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.08352484f588ef488586.bundle.js"
+  },
+  {
+    "url": "runtime~main.08352484f588ef488586.bundle.js"
+  },
+  {
+    "revision": "9ebb7e4151f427737ed1",
+    "url": "static/css/main.342981c4.chunk.css"
+  },
+  {
+    "url": "vendors~main.08352484f588ef488586.bundle.js"
+  },
+  {
+    "url": "vendors~main.08352484f588ef488586.bundle.js.LICENSE.txt"
+  }
+]);
